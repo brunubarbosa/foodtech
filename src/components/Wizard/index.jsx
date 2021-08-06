59dad5e354c8c4da8e7f3a7aa4842d2e85f2c773
brunubarbosa <brunopjsw@gmail.com>
2021-08-06 18:48:19 -0300

    :sparkles: styling wizard
    add color and format to wizar

--- src/components/Wizard/index.jsx
+++ src/components/Wizard/index.jsx
@@ -1,23 +1,27 @@
 import styles from './styles.module.scss';
+import {AiOutlineCheckCircle} from 'react-icons/ai'
 export const Wizard = () => {
   return (
   <div className={styles.wrapper}>
     <div className={styles.wrapper}>
       <span className={styles.itemWrapper}>
-        <span className={styles.item}>1</span>
+      <span className={`${styles.item} ${styles.past}`}>
+          <AiOutlineCheckCircle size="20" color="#fc5c86"/>
+        </span>
         Massa
       </span>
       <span className={styles.itemWrapper}>
-        <span className={styles.item}>1</span>
+        <span className={`${styles.item} ${styles.current}`}>2</span>
         Tamanho
       </span>
       <span className={styles.itemWrapper}>
-        <span className={styles.item}>1</span>
+        <span className={styles.item}>3</span>
         Sabor
       </span>
       <span className={styles.itemWrapper}>
-      <span className={styles.item}>1</span>
+      <span className={styles.item}>4</span>
         Pagamento
+        
       </span>
       </div>
   </div>
